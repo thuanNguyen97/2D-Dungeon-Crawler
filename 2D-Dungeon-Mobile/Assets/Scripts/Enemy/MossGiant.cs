ec0330c6b996793a753af331d9c4e30b372028cc
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-07-09 20:46:05 +0300

    Moss Giant Movement

--- 2D-Dungeon-Mobile/Assets/Scripts/Enemy/MossGiant.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/Enemy/MossGiant.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
+    private Vector3 _currentTarget;
+
     private void Start() 
     {
-        Attack();
-    }
-
-    public override void Attack()
-    {
-        base.Attack();
+        
     }
 
     public override void Update()
     {
-        Debug.Log("Moss Giant Updated ...");
+        Movement();
+    }
+
+    void Movement()
+    {
+        //if current pos == point A
+        //move to pointB
+        //else if current pos == point B
+        //move to pointA
+
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+            
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+            
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
     }
 }
