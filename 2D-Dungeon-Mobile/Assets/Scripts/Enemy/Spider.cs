f989d3f160b516ea5ebd58eaf03e6eb25c7ff4ef
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-07-11 13:28:29 +0300

    Spider AI Movement

--- 2D-Dungeon-Mobile/Assets/Scripts/Enemy/Spider.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/Enemy/Spider.cs
@@ -4,18 +4,60 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
+    private Vector3 _currentTarget;
+    private Animator _anim;
+    private SpriteRenderer _spiderSprite;
+
     private void Start()
     {
-        Attack();
-    }
-
-    public override void Attack()
-    {
-        Debug.Log("Spider Attack");
+        _anim = GetComponentInChildren<Animator>();
+        _spiderSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Update()
     {
-         Debug.Log("Spider updated ...");
+        //if idle is playing
+        //do nothing
+
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        //if current pos == pointA
+        //move right
+        //else if current == pointB
+        //move left
+
+        if (_currentTarget == pointA.position)
+        {
+            _spiderSprite.flipX = true;
+        }
+        else if (_currentTarget == pointB.position)
+        {
+            _spiderSprite.flipX = false;
+        }    
+
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+            _anim.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+            _anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+
+        //play idle when reach destination
+        //flip sprite
+
     }
 }
