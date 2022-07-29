ff9ccb604d07fe53abe4931bb0ea23c7a66b45dd
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-07-29 17:57:28 +0300

    Skeleton Damage Implementation

--- 2D-Dungeon-Mobile/Assets/Scripts/Enemy/Skeleton.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/Enemy/Skeleton.cs
@@ -9,10 +9,22 @@ public class Skeleton : Enemy, IDamageable
     public override void Init()
     {
         base.Init();
+        //assign Health property to our enemy health
+        Health = base.health;
     }
 
     public void Damage()
     {
         Debug.Log("Damage!");
+
+        //subtract 1 from health
+        Health--;
+
+        //if health less than 1
+        if (Health < 1)
+        {
+            //destroy
+            Destroy(this.gameObject);
+        }    
     }
 }
