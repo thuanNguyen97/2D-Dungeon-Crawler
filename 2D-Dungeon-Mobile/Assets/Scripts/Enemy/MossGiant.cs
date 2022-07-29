eb3ba7e3b4ea5e21cf5b48c128370c0d86b9cbd9
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-07-29 13:27:44 +0300

    IDamageable Interface

--- 2D-Dungeon-Mobile/Assets/Scripts/Enemy/MossGiant.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/Enemy/MossGiant.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MossGiant : Enemy
+public class MossGiant : Enemy, IDamageable
 {
+    //properties
+    public int Health { get; set;}
+
     //use for initialization
     public override void Init()
     {
         base.Init();
+
+        Health = base.health;
     } 
+
+       public void Damage()
+    {
+        
+    }
 }
