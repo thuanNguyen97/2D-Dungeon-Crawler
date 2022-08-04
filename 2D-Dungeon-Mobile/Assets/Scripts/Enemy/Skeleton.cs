cda7ab8afdd2bcc6855d54bc84029f2dde630413
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-08-04 15:19:20 +0300

    Skeleton Face player

--- 2D-Dungeon-Mobile/Assets/Scripts/Enemy/Skeleton.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/Enemy/Skeleton.cs
@@ -13,6 +13,30 @@ public class Skeleton : Enemy, IDamageable
         Health = base.health;
     }
 
+    public override void Movement()
+    {
+        base.Movement();
+
+        float distance = Vector3.Distance(player.transform.localPosition, transform.localPosition);
+
+        Debug.Log("Distance: " + distance);
+
+        Vector3 direction = player.transform.localPosition - transform.localPosition;
+
+        Debug.Log("Side: " + direction.x);
+
+        if (direction.x > 0 && anim.GetBool("InCombat") == true)
+        {
+            //face right
+            sprite.flipX = false;
+        }
+        else if (direction.x < 0 && anim.GetBool("InCombat") == true)
+        {
+            //face left
+            sprite.flipX = true;
+        }    
+    }
+
     public void Damage()
     {
         Debug.Log("Damage!");
