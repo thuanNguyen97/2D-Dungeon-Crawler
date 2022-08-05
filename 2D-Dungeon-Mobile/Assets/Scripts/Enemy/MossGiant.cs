e889f7896b7ba3b22df2d7a3a4f87cb39aa6e285
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-08-05 09:43:05 +0300

    Moss Giant Attack Complete

--- 2D-Dungeon-Mobile/Assets/Scripts/Enemy/MossGiant.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/Enemy/MossGiant.cs
@@ -13,10 +13,51 @@ public class MossGiant : Enemy, IDamageable
         base.Init();
 
         Health = base.health;
-    } 
+    }
 
-       public void Damage()
+    public override void Movement()
     {
-        
+        base.Movement();
+
+        float distance = Vector3.Distance(player.transform.localPosition, transform.localPosition);
+
+        //Debug.Log("Distance: " + distance);
+
+        Vector3 direction = player.transform.localPosition - transform.localPosition;
+
+        //Debug.Log("Side: " + direction.x);
+
+        if (direction.x > 0 && anim.GetBool("InCombat") == true)
+        {
+            //face right
+            sprite.flipX = false;
+        }
+        else if (direction.x < 0 && anim.GetBool("InCombat") == true)
+        {
+            //face left
+            sprite.flipX = true;
+        }
+    }
+
+    public void Damage()
+    {
+        Debug.Log("MossGiant::Damage!");
+
+        //subtract 1 from health
+        Health--;
+
+        //trigger hit anim
+        anim.SetTrigger("Hit");
+
+        isHit = true;
+
+        anim.SetBool("InCombat", true);
+
+        //if health less than 1
+        if (Health < 1)
+        {
+            //destroy
+            Destroy(this.gameObject);
+        }
     }
 }
