325ce3000728aff0e37eec8e2fa6e395ae8f16a8
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-08-06 22:00:34 +0300

    Spider animation Event

---
+++ 2D-Dungeon-Mobile/Assets/Scripts/Enemy/SpiderAnimationEvent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAnimationEvent : MonoBehaviour
+{
+    public void Fire()
+    {
+        //tell spider to fire
+        Debug.Log("Spider should fire");
+    }
+}
