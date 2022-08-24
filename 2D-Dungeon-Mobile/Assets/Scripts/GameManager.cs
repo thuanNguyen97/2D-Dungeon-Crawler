edb6ecef19de844ce2e2f356d931c24cfbe5f95d
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-08-24 23:59:25 +0300

    Update GameManager.cs

--- 2D-Dungeon-Mobile/Assets/Scripts/GameManager.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/GameManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _instance;
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("GameManager is null !!");
+
+            }   
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _instance = this;
     }
 }
