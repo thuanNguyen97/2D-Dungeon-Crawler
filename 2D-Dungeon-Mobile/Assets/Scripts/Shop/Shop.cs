0d1420e6551167a26eb88bb75cc3e525b05b5af2
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-08-24 17:55:26 +0300

    Implement Buy Item from shop

--- 2D-Dungeon-Mobile/Assets/Scripts/Shop/Shop.cs
+++ 2D-Dungeon-Mobile/Assets/Scripts/Shop/Shop.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     public GameObject shopPanel;
+    public int currentSelectedItem;
+    public int currentItemCost;
+
+    private Player _player;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Player player = other.GetComponent<Player>();
+            _player = other.GetComponent<Player>();
 
-            if (player != null)
+            if (_player != null)
             {
                 //send player diamond info to the UIManager
-                UIManager.Instance.OpenShop(player.diamonds);
+                UIManager.Instance.OpenShop(_player.diamonds);
             }    
 
             shopPanel.SetActive(true);
@@ -43,12 +47,18 @@ public class Shop : MonoBehaviour
         {
             case 0: //flame sword
                 UIManager.Instance.UpdateShopSelection(55);
+                currentSelectedItem = 0;
+                currentItemCost = 200;
                 break;
             case 1: //boots of flight
                 UIManager.Instance.UpdateShopSelection(-41);
+                currentSelectedItem = 1;
+                currentItemCost = 400;
                 break;
             case 2: //key to the castle
                 UIManager.Instance.UpdateShopSelection(-125);
+                currentSelectedItem = 2;
+                currentItemCost = 100;
                 break;
         }
     }
@@ -57,4 +67,21 @@ public class Shop : MonoBehaviour
     //check if player's gem is greater or equal to the item price
     //if it is, then got the item. subtract cost from players gem
     //else, cancel sale
+
+    public void BuyItem()
+    {
+        if (_player.diamonds >= currentItemCost)
+        {
+            //award item
+            //subtract money
+            _player.diamonds -= currentItemCost;
+
+            Debug.Log("Purchased " + currentSelectedItem);
+        }
+        else
+        {
+            Debug.Log("You do not have enough gem.");
+            shopPanel.SetActive(false);
+        }    
+    }    
 }
